96d8f0922987b0a566e2756d6bf4dc1d2e0b86fb
Bryan-source <bescobar34@yahoo.com>
2022-04-25 13:31:22 -0500

    Column Row

---
+++ components/Comp.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {StatusBar, Text, View} from 'react-native';
+import styles from './styles';
+import Box from './Box/Box';
+import Row from './Tools/Row';
+import Column from './Tools/Column';
+
+function Comp() {
+    return (
+        <View style = {styles.container}>
+            {/* <StatusBar hidden={true}/> */}
+            <Row>
+                <Column>
+                    <Box>#1</Box>
+                    <Box>#2</Box>
+                </Column>
+
+                <Column>
+                    <Box>#3</Box>
+                    <Box>#4</Box>
+                </Column>
+            </Row>
+
+            <Row>
+                <Column>
+                    <Box>#1</Box>
+                    <Box>#2</Box>
+                </Column>
+
+                <Column>
+                    <Box>#3</Box>
+                    <Box>#4</Box>
+                </Column>
+            </Row>
+        </View>
+    );
+}
+
+export default Comp;
+
+//ANGEL 0.3
